6af7e6bd474babb3955f61cc942ccb29e6df5ecd
bian <bxd_best@163.com>
2022-07-15 23:45:53 +0800

    update

--- universal_landmark_detection/model/datasets/__init__.py
+++ universal_landmark_detection/model/datasets/__init__.py
@@ -1,9 +1,9 @@
-#from __future__ import absolute_import#有限查找系统中的路径
+from __future__ import absolute_import#优先查找系统中的路径
 from . import *
 
-from .cephalometric import Cephalometric
-from .hand import Hand
-from .chest import Chest
+from model.datasets.cephalometric import Cephalometric
+from model.datasets.hand import Hand
+from model.datasets.chest import Chest
 
 def get_dataset(s):
     return {
